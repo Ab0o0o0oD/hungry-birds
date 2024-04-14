a055748d64e937df0918d56ea05d3e9424246df9
Abdulrazak Kanjo <abdulrazak.kanjo@gjensidige.no>
2024-04-14 23:16:56 +0200

    fix: Use jasypt secrets encryption

---
+++ hungry-birds-server/src/main/kotlin/com/restaurant/hungry/birds/config/AppConfig.kt
@@ -0,0 +1,8 @@
+package com.restaurant.hungry.birds.config
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class AppConfig
